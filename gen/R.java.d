4656ebdd31d1fb5adadab383aff596cbbe85ce37
Alexandre Skrzyniarz <alexandre.skrzyniarz@laposte.net>
2012-02-05 17:25:38 +0100

    remove generated sources from repository

--- gen/R.java.d
+++
@@ -1,7 +0,0 @@
-/mnt/hdb3/home/skrzynia/home/android/weird_dices/gen/net/nerim/skrzynia/weirddices/R.java \
- : /mnt/hdb3/home/skrzynia/home/android/weird_dices/res/values/strings.xml \
-/mnt/hdb3/home/skrzynia/home/android/weird_dices/res/values/dimentions.xml \
-/mnt/hdb3/home/skrzynia/home/android/weird_dices/res/values/colors.xml \
-/mnt/hdb3/home/skrzynia/home/android/weird_dices/res/layout/main.xml \
-/mnt/hdb3/home/skrzynia/home/android/weird_dices/res/drawable/d12.png \
-/mnt/hdb3/home/skrzynia/home/android/weird_dices/AndroidManifest.xml \
